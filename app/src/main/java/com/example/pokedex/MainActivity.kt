7b79f85f0c04328bcf2fdabe2c475216ae3207d9
JordiAllepuz <valencianu@gmail.com>
2024-01-02 17:03:05 +0100

    Inyectamos dagger hilt en el MainActivity y en la aplicacion, ademas añadimos la apliacion en el manifest. Aprovechamos que abrimos el manifest y le damos permisos para acceso a internet. Nos hara falta mas adelante para acceder a url que encontraremos en el Json. Como por ejemplo acceder a la imagen del pokemon.

--- app/src/main/java/com/example/pokedex/MainActivity.kt
+++ app/src/main/java/com/example/pokedex/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.pokedex
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -11,8 +12,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.pokedex.ui.theme.PokedexTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
