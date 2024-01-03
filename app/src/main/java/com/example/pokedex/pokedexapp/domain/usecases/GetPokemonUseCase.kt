5b465ff24e44b916084ad240927168314202f386
JordiAllepuz <valencianu@gmail.com>
2024-01-03 12:24:13 +0100

    UseCase (casos de uso), inyeccion depedencias con @Inject Constructor y pasandole el Repository como parametro.

--- app/src/main/java/com/example/pokedex/pokedexapp/domain/usecases/GetPokemonUseCase.kt
+++ app/src/main/java/com/example/pokedex/pokedexapp/domain/usecases/GetPokemonUseCase.kt
@@ -1,2 +1,14 @@
 package com.example.pokedex.pokedexapp.domain.usecases
 
+import com.example.pokedex.pokedexapp.data.dataInfo.Pokemon
+import com.example.pokedex.pokedexapp.domain.repositories.PokemonRepository
+import javax.inject.Inject
+
+class GetPokemoUseCase @Inject constructor(private val repository: PokemonRepository){
+
+    suspend fun execute (jsonNombre:String): Pokemon {
+        return repository.getPokemonFromJson(jsonNombre)
+    }
+
+
+}
