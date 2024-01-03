ca746fdae4e303138418a9e78748d6c3debbb01a
JordiAllepuz <valencianu@gmail.com>
2024-01-03 11:52:29 +0100

    Repository, inyeccion depedencias con @Inject Constructor y pasandole el DataSource como parametro.

--- app/src/main/java/com/example/pokedex/pokedexapp/domain/repositories/PokemonRepository.kt
+++ app/src/main/java/com/example/pokedex/pokedexapp/domain/repositories/PokemonRepository.kt
@@ -1,2 +1,24 @@
 package com.example.pokedex.pokedexapp.domain.repositories
 
+import android.content.Context
+import com.example.pokedex.pokedexapp.data.dataInfo.Pokemon
+import com.example.pokedex.pokedexapp.data.sources.local.PokemonLocalDataSource
+import javax.inject.Inject
+
+class PokemonRepository @Inject constructor(private val localDataSource: PokemonLocalDataSource) {
+
+    suspend fun getPokemonFromJson(jsonNombre: String): Pokemon {
+        return localDataSource.getPokemonFromJson(jsonNombre)
+    }
+
+    suspend fun readJsonFromAssets(context: Context, jsonNombre: String): String {
+        return localDataSource.readJsonFromAssets(context, jsonNombre)
+    }
+
+
+    suspend fun parseJsonToModel(jsonNombre: String): Pokemon {
+        return parseJsonToModel(jsonNombre)
+    }
+
+
+}
