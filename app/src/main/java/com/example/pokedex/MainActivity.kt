1efd24e3ce5f83e718537311bf3c7242183da8c6
JordiAllepuz <valencianu@gmail.com>
2024-01-03 14:57:21 +0100

    ViewModel, inyeccion depedencias con @HiltViewModel, y Inject constructor pasandole un caso de uso como parametro, pokemonUseCase Tengo una variable pokemon del tipo Pokemon y una funcion para cargar al "pokemon",  atraves del caso de uso la informacion leida del Json. Pasandole el nombre del Json por parametro. (Si añado mas json a assets como he hecho para comprobar el funcionamiento, es aqui donde hay que cambiar el nombre del Json para que lea uno u otro).

--- app/src/main/java/com/example/pokedex/MainActivity.kt
+++ app/src/main/java/com/example/pokedex/MainActivity.kt
@@ -7,10 +7,8 @@ import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.pokedex.pokedexapp.ui.viewmodels.PokemonViewModel
 import com.example.pokedex.ui.theme.PokedexTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -18,6 +16,7 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
+    private val pokemonViewModel: PokemonViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/example/pokedex/pokedexapp/ui/viewmodels/PokemonViewModel.kt
+++ app/src/main/java/com/example/pokedex/pokedexapp/ui/viewmodels/PokemonViewModel.kt
@@ -1,2 +1,29 @@
 package com.example.pokedex.pokedexapp.ui.viewmodels
 
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.pokedex.pokedexapp.data.dataInfo.Pokemon
+import com.example.pokedex.pokedexapp.domain.usecases.GetPokemoUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class PokemonViewModel @Inject constructor(private val getPokemonUseCase: GetPokemoUseCase):
+    ViewModel(){
+
+    var pokemon = mutableStateOf<Pokemon?>(null)
+        private set
+
+    init {
+        loadPokemon("ditto.json")
+    }
+
+    private fun loadPokemon(jsonNombre: String) {
+        viewModelScope.launch {
+            pokemon.value = getPokemonUseCase.execute(jsonNombre)
+        }
+    }
+
+}
