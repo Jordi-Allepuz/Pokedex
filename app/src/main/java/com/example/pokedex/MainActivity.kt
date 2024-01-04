92c65240aedcae9e403538ec6771a9d4d04b7a23
JordiAllepuz <valencianu@gmail.com>
2024-01-04 12:43:35 +0100

    Creando Composables para la vista. Creo la pantalla principal para añadirla al Main Activity. En la pantalla principal ire añadiendo los composables de cada parte de la pantalla. Añado otras funciones (solo añado el nombre) donde se mostraran los tipos, el peso y la altura y para terminar los Stats. Mas adelante se crearan dichas Funciones Composables.

--- app/src/main/java/com/example/pokedex/MainActivity.kt
+++ app/src/main/java/com/example/pokedex/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import com.example.pokedex.pokedexapp.ui.screens.PokemonPantallaFicha
 import com.example.pokedex.pokedexapp.ui.viewmodels.PokemonViewModel
 import com.example.pokedex.ui.theme.PokedexTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -27,7 +28,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    PokemonPantallaFicha(pokemonViewModel)
                 }
             }
         }
--- app/src/main/java/com/example/pokedex/pokedexapp/ui/screens/PokemonFichaScreen.kt
+++ app/src/main/java/com/example/pokedex/pokedexapp/ui/screens/PokemonFichaScreen.kt
@@ -1,6 +1,8 @@
 package com.example.pokedex.pokedexapp.ui.screens
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -30,6 +32,66 @@ import androidx.compose.ui.unit.sp
 import coil.annotation.ExperimentalCoilApi
 import coil.compose.rememberImagePainter
 import com.example.pokedex.pokedexapp.data.dataInfo.Pokemon
+import com.example.pokedex.pokedexapp.ui.viewmodels.PokemonViewModel
+
+
+@Composable
+fun PokemonPantallaFicha(viewModel: PokemonViewModel) {
+
+    val pokemon = viewModel.pokemon.value!!
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFF272727)),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Cabecera(pokemon = pokemon)
+        Spacer(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(16.dp)
+        )
+        Text(
+            text = pokemon.name,
+            color = Color.White,
+            fontWeight = FontWeight.Bold,
+            fontSize = 40.sp,
+            modifier = Modifier.padding(10.dp)
+        )
+        Spacer(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(8.dp)
+        )
+        Tipos(pokemon = pokemon)
+        Spacer(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(25.dp)
+        )
+        PesoAltura(pokemon = pokemon)
+        Spacer(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(30.dp)
+        )
+        Text(
+            text = "Base Stats",
+            color = Color.White,
+            fontWeight = FontWeight.Bold,
+            fontSize = 30.sp,
+            modifier = Modifier.padding(4.dp)
+        )
+        Stats(pokemon = pokemon)
+    }
+}
+
+
+
+
+
 
 @OptIn(ExperimentalCoilApi::class)
 @Composable
